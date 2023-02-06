157e37c20b6a2b76d18dec0638c59b13485668d8
Hyuni02 <jackjsh@naver.com>
2023-02-07 00:58:12 +0900

    가방 추가
    Bag.cs

---
+++ Assets/Scripts/item/Bag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bag : Item
+{
+    [Header("Bag Info")]
+    public float Max_Size;
+
+    [Header("Total State")]
+    public float Total_Size;
+    public float Total_Weight;
+}
